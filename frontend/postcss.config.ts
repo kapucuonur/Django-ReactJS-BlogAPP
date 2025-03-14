294879d317432316d2c71c16aaa2d27c96bae731
Onur Kapucu <kapucuonur@hotmail.com>
2025-03-15 00:17:34 +0200

    tailwind updates

--- frontend/postcss.config.ts
+++ frontend/postcss.config.ts
@@ -1,8 +1,14 @@
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
     },
-  };
-  
-  
+  },
+});
