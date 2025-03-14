55fd3e5561ca7d5338c2eb8af15b8629c8a4fe2f
Onur Kapucu <kapucuonur@hotmail.com>
2025-03-15 00:43:35 +0200

    updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
+  define: {
+    'process.env': process.env,
   },
 });
